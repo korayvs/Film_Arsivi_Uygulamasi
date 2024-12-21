ec7ec2801bcea5b8e81cffb70106319c0a513e98
Koray <Koray@DESKTOP-F1A12T8>
2024-12-21 18:57:35 +0300

    Add project files.

---
+++ Film_Arsivim/Form1.Designer.cs
@@ -0,0 +1,379 @@
+﻿namespace Film_Arsivim
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.PnlBrowser = new System.Windows.Forms.Panel();
+            this.PnlTop = new System.Windows.Forms.Panel();
+            this.CmbAdresler = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.TxtAdresSatiri = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.BtnCikis = new System.Windows.Forms.Button();
+            this.BtnRenkDeg = new System.Windows.Forms.Button();
+            this.BtnHak = new System.Windows.Forms.Button();
+            this.BtnTamEkr = new System.Windows.Forms.Button();
+            this.BtnKaydet = new System.Windows.Forms.Button();
+            this.TxtLink = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TxtKategori = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtFilmAd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.PnlTop.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(3, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1103, 74);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Script", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(347, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(353, 40);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "SİNEMA KOLTUK YAZILIM";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(1019, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(82, 68);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(251, 79);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(262, 474);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Film Listesi";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 22);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(256, 449);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.PnlBrowser);
+            this.groupBox2.Controls.Add(this.PnlTop);
+            this.groupBox2.Location = new System.Drawing.Point(519, 79);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(584, 474);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ekran";
+            // 
+            // PnlBrowser
+            // 
+            this.PnlBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PnlBrowser.Location = new System.Drawing.Point(3, 68);
+            this.PnlBrowser.Name = "PnlBrowser";
+            this.PnlBrowser.Size = new System.Drawing.Size(578, 403);
+            this.PnlBrowser.TabIndex = 1;
+            // 
+            // PnlTop
+            // 
+            this.PnlTop.BackColor = System.Drawing.Color.Navy;
+            this.PnlTop.Controls.Add(this.button2);
+            this.PnlTop.Controls.Add(this.CmbAdresler);
+            this.PnlTop.Controls.Add(this.button1);
+            this.PnlTop.Controls.Add(this.TxtAdresSatiri);
+            this.PnlTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PnlTop.Location = new System.Drawing.Point(3, 22);
+            this.PnlTop.Name = "PnlTop";
+            this.PnlTop.Size = new System.Drawing.Size(578, 46);
+            this.PnlTop.TabIndex = 0;
+            // 
+            // CmbAdresler
+            // 
+            this.CmbAdresler.FormattingEnabled = true;
+            this.CmbAdresler.Items.AddRange(new object[] {
+            "www.youtube.com",
+            "www.instagram.com",
+            "www.facebook.com"});
+            this.CmbAdresler.Location = new System.Drawing.Point(357, 8);
+            this.CmbAdresler.Name = "CmbAdresler";
+            this.CmbAdresler.Size = new System.Drawing.Size(171, 28);
+            this.CmbAdresler.TabIndex = 2;
+            this.CmbAdresler.SelectedIndexChanged += new System.EventHandler(this.CmbAdresler_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(274, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(53, 26);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Git";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TxtAdresSatiri
+            // 
+            this.TxtAdresSatiri.Location = new System.Drawing.Point(7, 8);
+            this.TxtAdresSatiri.Name = "TxtAdresSatiri";
+            this.TxtAdresSatiri.Size = new System.Drawing.Size(261, 26);
+            this.TxtAdresSatiri.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.groupBox4);
+            this.groupBox3.Controls.Add(this.BtnKaydet);
+            this.groupBox3.Controls.Add(this.TxtLink);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.TxtKategori);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.TxtFilmAd);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Location = new System.Drawing.Point(3, 82);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(242, 471);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Yeni Film";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.BtnCikis);
+            this.groupBox4.Controls.Add(this.BtnRenkDeg);
+            this.groupBox4.Controls.Add(this.BtnHak);
+            this.groupBox4.Controls.Add(this.BtnTamEkr);
+            this.groupBox4.Location = new System.Drawing.Point(6, 226);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(230, 239);
+            this.groupBox4.TabIndex = 9;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "İşlemler";
+            // 
+            // BtnCikis
+            // 
+            this.BtnCikis.Location = new System.Drawing.Point(6, 166);
+            this.BtnCikis.Name = "BtnCikis";
+            this.BtnCikis.Size = new System.Drawing.Size(218, 41);
+            this.BtnCikis.TabIndex = 3;
+            this.BtnCikis.Text = "ÇIKIŞ";
+            this.BtnCikis.UseVisualStyleBackColor = true;
+            this.BtnCikis.Click += new System.EventHandler(this.BtnCikis_Click);
+            // 
+            // BtnRenkDeg
+            // 
+            this.BtnRenkDeg.Location = new System.Drawing.Point(6, 119);
+            this.BtnRenkDeg.Name = "BtnRenkDeg";
+            this.BtnRenkDeg.Size = new System.Drawing.Size(218, 41);
+            this.BtnRenkDeg.TabIndex = 2;
+            this.BtnRenkDeg.Text = "RENK DEĞİŞTİR";
+            this.BtnRenkDeg.UseVisualStyleBackColor = true;
+            this.BtnRenkDeg.Click += new System.EventHandler(this.BtnRenkDeg_Click);
+            // 
+            // BtnHak
+            // 
+            this.BtnHak.Location = new System.Drawing.Point(7, 72);
+            this.BtnHak.Name = "BtnHak";
+            this.BtnHak.Size = new System.Drawing.Size(217, 41);
+            this.BtnHak.TabIndex = 1;
+            this.BtnHak.Text = "HAKKIMIZDA";
+            this.BtnHak.UseVisualStyleBackColor = true;
+            this.BtnHak.Click += new System.EventHandler(this.BtnHak_Click);
+            // 
+            // BtnTamEkr
+            // 
+            this.BtnTamEkr.Location = new System.Drawing.Point(7, 25);
+            this.BtnTamEkr.Name = "BtnTamEkr";
+            this.BtnTamEkr.Size = new System.Drawing.Size(217, 41);
+            this.BtnTamEkr.TabIndex = 0;
+            this.BtnTamEkr.Text = "TAM EKRAN";
+            this.BtnTamEkr.UseVisualStyleBackColor = true;
+            this.BtnTamEkr.Click += new System.EventHandler(this.BtnTamEkr_Click);
+            // 
+            // BtnKaydet
+            // 
+            this.BtnKaydet.Location = new System.Drawing.Point(89, 138);
+            this.BtnKaydet.Name = "BtnKaydet";
+            this.BtnKaydet.Size = new System.Drawing.Size(147, 36);
+            this.BtnKaydet.TabIndex = 8;
+            this.BtnKaydet.Text = "Kaydet";
+            this.BtnKaydet.UseVisualStyleBackColor = true;
+            this.BtnKaydet.Click += new System.EventHandler(this.BtnKaydet_Click);
+            // 
+            // TxtLink
+            // 
+            this.TxtLink.Location = new System.Drawing.Point(89, 106);
+            this.TxtLink.Name = "TxtLink";
+            this.TxtLink.Size = new System.Drawing.Size(147, 26);
+            this.TxtLink.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(41, 109);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Link:";
+            // 
+            // TxtKategori
+            // 
+            this.TxtKategori.Location = new System.Drawing.Point(89, 74);
+            this.TxtKategori.Name = "TxtKategori";
+            this.TxtKategori.Size = new System.Drawing.Size(147, 26);
+            this.TxtKategori.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Kategori:";
+            // 
+            // TxtFilmAd
+            // 
+            this.TxtFilmAd.Location = new System.Drawing.Point(89, 42);
+            this.TxtFilmAd.Name = "TxtFilmAd";
+            this.TxtFilmAd.Size = new System.Drawing.Size(147, 26);
+            this.TxtFilmAd.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Film Ad:";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.ForeColor = System.Drawing.Color.Transparent;
+            this.button2.Location = new System.Drawing.Point(538, 7);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(32, 28);
+            this.button2.TabIndex = 3;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SandyBrown;
+            this.ClientSize = new System.Drawing.Size(1109, 557);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.PnlTop.ResumeLayout(false);
+            this.PnlTop.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button BtnCikis;
+        private System.Windows.Forms.Button BtnRenkDeg;
+        private System.Windows.Forms.Button BtnHak;
+        private System.Windows.Forms.Button BtnTamEkr;
+        private System.Windows.Forms.Button BtnKaydet;
+        private System.Windows.Forms.TextBox TxtLink;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TxtKategori;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtFilmAd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel PnlBrowser;
+        private System.Windows.Forms.Panel PnlTop;
+        private System.Windows.Forms.ComboBox CmbAdresler;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox TxtAdresSatiri;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Film_Arsivim/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace Film_Arsivim
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection baglanti = new SqlConnection(@"Data Source=DESKTOP-F1A12T8\KORAY;Initial Catalog=FilmArsivim;Integrated Security=True");
+
+        void filmler()
+        {
+            SqlDataAdapter da = new SqlDataAdapter("Select * From TBLFILMLER", baglanti);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+        
+        ChromiumWebBrowser tarayici;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            filmler();
+
+            tarayici = new ChromiumWebBrowser(); 
+            this.PnlBrowser.Controls.Add(tarayici);                                                      
+            tarayici.Dock = DockStyle.Fill;
+            
+        }
+
+        private void BtnKaydet_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("Insert into TBLFILMLER (AD, KATEGORI, LINK) Values (@p1, @p2, @p3)", baglanti);
+            komut.Parameters.AddWithValue("@p1", TxtFilmAd.Text);
+            komut.Parameters.AddWithValue("@p2", TxtKategori.Text);
+            komut.Parameters.AddWithValue("@p3", TxtLink.Text);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Film listeniz eklendi", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            filmler();
+        }
+        ChromiumWebBrowser chromiumWebBrowser;
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int secilen = dataGridView1.SelectedCells[0].RowIndex;
+            string link = dataGridView1.Rows[secilen].Cells[3].Value.ToString();
+            chromiumWebBrowser = new ChromiumWebBrowser(link);
+            this.groupBox2.Controls.Add(chromiumWebBrowser);
+            chromiumWebBrowser.Dock = DockStyle.Fill;
+            chromiumWebBrowser.Load(link);
+        }
+
+        private void BtnHak_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bu proje Koray Vatansever tarafından 26.11.2024'de kodlandı", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void BtnCikis_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void BtnRenkDeg_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            int red = rand.Next(0, 255);
+            int green = rand.Next(0, 255);
+            int blue = rand.Next(0, 255);
+
+            this.BackColor = Color.FromArgb(red, green, blue);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            tarayici.Load("www.youtube.com");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tarayici.Load(TxtAdresSatiri.Text);
+        }
+
+        private void CmbAdresler_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            tarayici.Load(CmbAdresler.SelectedItem.ToString());
+        }
+
+        private void BtnTamEkr_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            groupBox2.Size = new Size(1920, 1080);
+            groupBox2.Location = new Point(0, 0);
+        }
+    }
+}
